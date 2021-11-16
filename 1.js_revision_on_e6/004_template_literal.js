f60bbf438936e23560c7fc7b31f7b40165310c9e
jeraldpraveen <jerald.vit@gmail.com>
2021-11-16 20:12:02 +0530

    3. Template literal

---
+++ 1.js_revision_on_e6/004_template_literal.js
@@ -0,0 +1,6 @@
+console.log("Hello All");
+
+const myName = "Jerald";
+// console.log('I'm ' + myName + ".Nice To Meet You");
+console.log("I'm " + myName + ".Nice To Meet You");
+console.log(`I'm ${myName}.Nice To Meet You`);
