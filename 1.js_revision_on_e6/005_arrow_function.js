52a0b0351987aa1ceafea137d28461ae906442bd
jeraldpraveen <jerald.vit@gmail.com>
2021-11-16 20:20:57 +0530

    4. Arrow function

---
+++ 1.js_revision_on_e6/005_arrow_function.js
@@ -0,0 +1,14 @@
+function sayMyName(myName) {
+  return `My name is ${myName}`;
+}
+
+console.log(sayMyName("Jerald"));
+
+const sayMyName2 = (myName) => {
+  return `My name is ${myName}`;
+};
+
+console.log(sayMyName2("Jerald"));
+
+const sayMyName3 = (myName) => `My name is ${myName}`;
+console.log(sayMyName3("Jerald"));
