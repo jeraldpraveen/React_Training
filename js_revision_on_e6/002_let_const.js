94647cafeae4b76a9119303b2ad0eec6fdf4e60d
jeraldpraveen <jerald.vit@gmail.com>
2021-11-16 19:51:32 +0530

    1.Let Const commit

---
+++ js_revision_on_e6/002_let_const.js
@@ -0,0 +1,16 @@
+// let
+let age = 13;
+
+if (true) {
+  let age = 27;
+  console.log(age);
+}
+console.log(age);
+
+// const
+const constNumber = 5;
+// myNumber = 10;
+
+const fruits = ["apple"];
+fruits.push("orange");
+console.log(fruits);
