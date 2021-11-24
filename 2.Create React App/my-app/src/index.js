68c9e6a4c0df98fe7904a881969091f41acc33ea
jeraldpraveen <jerald.vit@gmail.com>
2021-11-24 13:01:05 +0530

    11.Create React App & JSX Basics

---
+++ 2.Create React App/my-app/src/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+// ReactDOM.render(
+//   React.createElement("h1", {}, "sdafsdf"),
+//   document.getElementById("root")
+// );
+
+// var div = React.createElement(
+//   "div",
+//   { id: "jer", className: "center" },
+//   React.createElement("h1", {}, "sdafsdf")
+// );
+// ReactDOM.render(div, document.getElementById("root"));
+
+// function App1() {
+//   return (
+//     <div id="jer" className="center">
+//       <h1>dfsghdfh</h1>
+//     </div>
+//   );
+// }
+
+// ReactDOM.render(<App1 />, document.getElementById("root"));
+
+ReactDOM.render(<App />, document.getElementById("root"));
