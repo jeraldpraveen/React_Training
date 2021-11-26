a9b9f7fdf064f9dd58dad62d66a48fc44743b295
jeraldpraveen <jerald.vit@gmail.com>
2021-11-26 14:46:04 +0530

    21.Splitting Of Components

--- expense_tracker/src/components/ExpenseItem.js
+++ expense_tracker/src/components/ExpenseItem.js
@@ -1,19 +1,12 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-  // const expenseDate = props.date.toString();
-  const month = props.date.toLocaleString("en-US", { month: "long" });
-  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-  const year = props.date.getFullYear();
   const expenseTitle = props.title;
   const expenseAmount = props.amount;
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{expenseTitle}</h2>
         <div className="expense-item__price">₹{expenseAmount}</div>
