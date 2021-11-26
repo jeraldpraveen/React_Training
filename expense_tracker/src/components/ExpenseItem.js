e872abdb83e7fc116f2103c4db6ba62ab0992d3e
jeraldpraveen <jerald.vit@gmail.com>
2021-11-26 14:32:30 +0530

    20.Adding JS Logic To Split Date

--- expense_tracker/src/components/ExpenseItem.js
+++ expense_tracker/src/components/ExpenseItem.js
@@ -1,12 +1,19 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-  const expenseDate = props.date.toString();
+  // const expenseDate = props.date.toString();
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
   const expenseTitle = props.title;
   const expenseAmount = props.amount;
   return (
     <div className="expense-item">
-      <div>{expenseDate}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{expenseTitle}</h2>
         <div className="expense-item__price">₹{expenseAmount}</div>
