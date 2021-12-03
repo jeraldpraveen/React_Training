2e28a733989a75efd93f20b87ae5d15799ecf974
jeraldpraveen <jerald.vit@gmail.com>
2021-12-03 15:08:41 +0530

    34.Working With Multiple States

--- expense_tracker/src/components/NewExpense/ExpenseForm.js
+++ expense_tracker/src/components/NewExpense/ExpenseForm.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-  //   document.getElementById("someId").addEventListener("change", (event) => {});
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
   const titleChangeHandler = (event) => {
-    // console.log(event);
-    // We get current Value Of Input Field With every Key Stroke
-    console.log(event.target.value);
+    setEnteredTitle(event.target.value);
+  };
+
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
   };
 
   return (
@@ -18,11 +27,16 @@ const ExpenseForm = () => {
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" />
+          <input type="number" onChange={amountChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-31" />
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
