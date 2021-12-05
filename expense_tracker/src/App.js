10552cc6497d197d56f27b9fd8e284a3d21361c8
jeraldpraveen <jerald.vit@gmail.com>
2021-12-06 00:36:43 +0530

    40.Lifting State Up and Dynamic Data Rendering

--- expense_tracker/src/App.js
+++ expense_tracker/src/App.js
@@ -1,35 +1,36 @@
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toll Gate",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  {
+    id: "e2",
+    title: "New TV",
+    amount: 799.49,
+    date: new Date(2021, 2, 12),
+  },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "Laptop",
+    amount: 45000,
+    date: new Date(2021, 5, 12),
+  },
+];
 function App() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toll Gate",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    {
-      id: "e2",
-      title: "New TV",
-      amount: 799.49,
-      date: new Date(2021, 2, 12),
-    },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "Laptop",
-      amount: 45000,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+  const [expensesList, setExpenseList] = useState(DUMMY_EXPENSES);
 
   // return React.createElement(
   //   "div",
@@ -40,14 +41,17 @@ function App() {
 
   const addExpenseHandler = (expense) => {
     console.log("App.js");
-    console.log(expense);
+    setExpenseList((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+    console.log(expensesList);
   };
 
   return (
     <div className="App">
       <h1>Expense Tracker</h1>
       <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses items={expenses} />
+      <Expenses items={expensesList} />
     </div>
   );
 }
