86af19d331611d21d9dda3bba711f9e5b1fdd41a
jeraldpraveen <jerald.vit@gmail.com>
2021-12-10 15:55:25 +0530

    47.Counter Operation Implementation

--- counter_class_based_app/src/components/ClassBased.js
+++ counter_class_based_app/src/components/ClassBased.js
@@ -2,14 +2,33 @@ import React, { Component } from "react";
 import "./ClassBased.css";
 
 class ClassBased extends Component {
+  constructor() {
+    super();
+    this.state = {
+      number: 0,
+    };
+  }
+
+  add = () => {
+    this.setState({ number: this.state.number + 1 });
+  };
+
+  sub = () => {
+    this.state.number > 0 && this.setState({ number: this.state.number - 1 });
+  };
+
   render() {
     return (
       <div className="counter">
         <h1>Counter Application</h1>
-        <div className="counter-display">1</div>
+        <div className="counter-display">{this.state.number}</div>
         <br />
-        <button className="counter-button">Add</button>
-        <button className="counter-button">Sub</button>
+        <button className="counter-button" onClick={this.add}>
+          Add
+        </button>
+        <button className="counter-button" onClick={this.sub}>
+          Sub
+        </button>
       </div>
     );
   }
