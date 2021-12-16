7bf512c6f6938b96659bc96b1f0c5e2448a5f7ae
jeraldpraveen <jerald.vit@gmail.com>
2021-12-17 04:26:58 +0530

    67.Refactoring To Double Quotes

--- 07-user-authentication/src/App.js
+++ 07-user-authentication/src/App.js
@@ -1,15 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
     setIsLoggedIn(true);
   };
 
