b1f5238416cc21c4fce39d23812cb7cae48cbb3e
jeraldpraveen <jerald.vit@gmail.com>
2021-12-24 08:30:34 +0530

    86.ContextProvider & Context Class

---
+++ 08-movies-application/src/context/MoviesContext.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export const MoviesProvider = React.createContext();
+
+export class MoviesContext extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default MoviesContext;
