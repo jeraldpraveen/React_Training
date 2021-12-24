5d17c3af80b3372c3b93c4a0c967f0961422910e
jeraldpraveen <jerald.vit@gmail.com>
2021-12-24 15:47:16 +0530

    99.MoviesList Updated with ContextAPI

--- 09-movies-list/src/components/MoviesList.js
+++ 09-movies-list/src/components/MoviesList.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
+import { MoviesContext } from "../context/MoviesContext";
 
 const MoviesList = () => {
+  const context = useContext(MoviesContext);
+
+  const moviesList = context.movies.map((movie) => {
+    return (
+      <div>
+        <h1>The Movie Name is {movie.movieName}</h1>
+        <h1>The Director Name is {movie.directorName}</h1>
+      </div>
+    );
+  });
   return (
     <div style={{ backgroundColor: "greenyellow" }}>
       <h1>MoviesList</h1>
+      {moviesList}
     </div>
   );
 };
