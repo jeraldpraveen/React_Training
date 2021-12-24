96ffd339b7c92e718a5f099aab787b8b445939a3
jeraldpraveen <jerald.vit@gmail.com>
2021-12-24 14:28:10 +0530

    91.Button & CSS for component Manipulation

--- 08-movies-application/src/components/A.js
+++ 08-movies-application/src/components/A.js
@@ -4,9 +4,10 @@ import { MoviesContext } from "../context/MoviesContext";
 const A = () => {
   const stateObject = useContext(MoviesContext);
   return (
-    <div>
+    <div style={{ backgroundColor: "red" }}>
       <h1>This is A Component</h1>
       <h1>The director name is {stateObject.director}</h1>
+      <button>Change Movie Name</button>
     </div>
   );
 };
--- 08-movies-application/src/components/B.js
+++ 08-movies-application/src/components/B.js
@@ -4,9 +4,10 @@ import { MoviesContext } from "../context/MoviesContext";
 const B = () => {
   const stateObject = useContext(MoviesContext);
   return (
-    <div>
+    <div style={{ backgroundColor: "green" }}>
       <h1>This is B Component</h1>
       <h1>The movie name is {stateObject.movie}</h1>
+      <button>Change Director</button>
     </div>
   );
 };
