9db78ba43e274f317d5753566e17a28501d89b48
jeraldpraveen <jerald.vit@gmail.com>
2021-12-24 14:55:00 +0530

    95.Component Creation for ContextAPI

--- 09-movies-list/src/App.js
+++ 09-movies-list/src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
+import MoviesList from "./components/MoviesList";
+import AddMovie from "./components/AddMovie";
 
 function App() {
   return (
     <div className="App">
       <h1>Movies List</h1>
+      <Navbar />
+      <AddMovie />
+      <MoviesList />
     </div>
   );
 }
---
+++ 09-movies-list/src/components/AddMovie.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AddMovie = () => {
+  return (
+    <div style={{ backgroundColor: "gray" }}>
+      <h1>AddMovie</h1>
+    </div>
+  );
+};
+
+export default AddMovie;
