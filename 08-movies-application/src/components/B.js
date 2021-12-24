c3f47ea4e5e4556eba05dbf6621b7477e3d8ea3f
jeraldpraveen <jerald.vit@gmail.com>
2021-12-24 11:08:42 +0530

    89.Fetching Value from useContext()

--- 08-movies-application/src/components/B.js
+++ 08-movies-application/src/components/B.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
+import { MoviesContext } from "../context/MoviesContext";
 
 const B = () => {
+  const stateObject = useContext(MoviesContext);
   return (
     <div>
       <h1>This is B Component</h1>
+      <h1>{stateObject.director}</h1>
     </div>
   );
 };
