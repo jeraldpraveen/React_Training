a60b343aee34d3cda6c795e41bf1e33d904c033c
jeraldpraveen <jerald.vit@gmail.com>
2021-12-24 10:23:39 +0530

    87.Child Component & State

--- 08-movies-application/src/App.js
+++ 08-movies-application/src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
+import A from "./components/A";
+import B from "./components/B";
 
 function App() {
   return (
     <div className="App">
       <h1>Context API</h1>
+      <A />
+      <B />
     </div>
   );
 }
--- 08-movies-application/src/context/MoviesContext.js
+++ 08-movies-application/src/context/MoviesContext.js
@@ -3,6 +3,14 @@ import React, { Component } from "react";
 export const MoviesProvider = React.createContext();
 
 export class MoviesContext extends Component {
+  constructor() {
+    super();
+    this.state = {
+      director: "Shankar",
+      movie: "Gentleman",
+    };
+  }
+
   render() {
     return <div></div>;
   }
