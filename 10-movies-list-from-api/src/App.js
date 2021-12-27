9acaaf3401ac2d715974c7affccb2b538d07dd46
jeraldpraveen <jerald.vit@gmail.com>
2021-12-27 22:21:18 +0530

    105.Handling HTTP Errors -->try/catch

--- 10-movies-list-from-api/src/App.js
+++ 10-movies-list-from-api/src/App.js
@@ -6,20 +6,28 @@ import "./App.css";
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   const fetchMoviesHandler = async () => {
-    setIsLoading(true);
-    const response = await fetch("https://swapi.dev/api/film/");
-    const data = await response.json();
-    const transformedMovies = data.results.map((movieData) => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date,
-      };
-    });
-    setMovies(transformedMovies);
+    try {
+      setIsLoading(true);
+      const response = await fetch("https://swapi.dev/api/film/");
+      if (response.status === 404) {
+        throw new Error("Something Is Wrong!");
+      }
+      const data = await response.json();
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+    } catch (error) {
+      setError(error.message);
+    }
     setIsLoading(false);
   };
 
