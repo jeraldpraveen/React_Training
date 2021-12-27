fc03ffb1b843a60b443af093fc80431bf1d5a290
jeraldpraveen <jerald.vit@gmail.com>
2021-12-27 22:31:30 +0530

    107.Handling Errors Part-3

--- 10-movies-list-from-api/src/App.js
+++ 10-movies-list-from-api/src/App.js
@@ -11,7 +11,7 @@ function App() {
   const fetchMoviesHandler = async () => {
     try {
       setIsLoading(true);
-      const response = await fetch("https://swapi.dev/api/film/");
+      const response = await fetch("https://swapi.dev/api/films/");
       if (response.status === 404) {
         throw new Error("Something Is Wrong!");
       }
@@ -33,17 +33,26 @@ function App() {
     setIsLoading(false);
   };
 
+  let content = <p>Found no movies.</p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
+
   return (
     <React.Fragment>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
-      <section>
-        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && !error && <p>No Movies Found</p>}
-        {!isLoading && error && <p>{error}</p>}
-        {isLoading && <p>Loading...</p>}
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
