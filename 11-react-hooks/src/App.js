13dd3ee07e329f9eef82bace328d632d034f4e05
jeraldpraveen <jerald.vit@gmail.com>
2021-12-31 12:40:45 +0530

    119.useCallback() with Dependencies

--- 11-react-hooks/src/App.js
+++ 11-react-hooks/src/App.js
@@ -6,17 +6,25 @@ import "./App.css";
 
 function App() {
   const [showPara, setShowPara] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
   console.log("APP RUNNING");
 
   const toggleParaHandler = useCallback(() => {
-    setShowPara((prevShowPara) => !prevShowPara);
-  }, []);
+    if (allowToggle) {
+      setShowPara((prevShowPara) => !prevShowPara);
+    }
+  }, [allowToggle]);
+
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
+  };
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <DemoOutput show={false} />
+      <DemoOutput show={showPara} />
+      <Button onClick={allowToggleHandler}>Allow Toggle</Button>
       <Button onClick={toggleParaHandler}>Para Hide/Show</Button>
     </div>
   );
@@ -29,3 +37,7 @@ export default App;
 // obj1 === obj2;
 //obj2 = obj1;
 // obj1 === obj2;
+
+// The Closure Compiler compiles JavaScript into compact, high-performance code.
+// The compiler removes dead code and rewrites and minimizes what's left so that it downloads and runs quickly.
+//  It also checks syntax, variable references, and types, and warns about common JavaScript pitfalls.
