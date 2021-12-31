35a62fd3196010b7a746e2f6f6dd335b9e2a0bcb
jeraldpraveen <jerald.vit@gmail.com>
2021-12-31 10:34:34 +0530

    Child Component Re-Evaluvation Part-1

--- 11-react-hooks/src/App.js
+++ 11-react-hooks/src/App.js
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
 import Button from "./components/UI/Button/Button";
+import DemoOutput from "./components/Demo/DemoOutput";
 
 import "./App.css";
 
 function App() {
   const [showPara, setShowPara] = useState(false);
+
+  console.log("APP RUNNING");
 
   const toggleParaHandler = () => {
     setShowPara((prevShowPara) => !prevShowPara);
@@ -13,7 +16,7 @@ function App() {
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      {showPara && <p>This is NEW!!!!</p>}
+      <DemoOutput show={showPara} />
       <Button onClick={toggleParaHandler}>Para Hide/Show</Button>
     </div>
   );
