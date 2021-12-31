455880dc26db07e20c3cd420f770d1e5e10f0fa6
jeraldpraveen <jerald.vit@gmail.com>
2021-12-31 10:15:58 +0530

    112.Component Update In Action

--- 11-react-hooks/src/App.js
+++ 11-react-hooks/src/App.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "./components/UI/Button/Button";
 
 import "./App.css";
 
 function App() {
+  const [showPara, setShowPara] = useState(false);
+
+  const toggleParaHandler = () => {
+    setShowPara((prevShowPara) => !prevShowPara);
+  };
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      {showPara && <p>This is NEW!!!!</p>}
+      <Button onClick={toggleParaHandler}>Para Hide/Show</Button>
     </div>
   );
 }
