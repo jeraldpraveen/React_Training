ac9f888469a9002dcb50c435049ea48b25e401a0
jeraldpraveen <jerald.vit@gmail.com>
2022-01-03 16:04:47 +0530

    125.Router Components

--- 12-react-router/src/App.js
+++ 12-react-router/src/App.js
@@ -1,9 +1,17 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import ContactUs from "./components/ContactUs";
+import Services from "./components/Services";
 
 function App() {
   return (
     <div className="App">
       <h1>REACT ROUTER</h1>
+      <Navbar />
+      <Home />
+      <ContactUs />
+      <Services />
     </div>
   );
 }
