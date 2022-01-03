73bcbb57af7d40506e53acc31b2242197656bd19
jeraldpraveen <jerald.vit@gmail.com>
2022-01-03 16:39:21 +0530

    132.axios.get()

--- 12-react-router/src/components/Services.js
+++ 12-react-router/src/components/Services.js
@@ -1,9 +1,23 @@
 import React from "react";
+import axios from "axios";
 
 const Services = () => {
+  const getDataUsingAxios = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div>
       <h1>Services</h1>
+      <h1>HTTP METHODS</h1>
+      <button onClick={getDataUsingAxios}>Get Data Using Axios</button>
     </div>
   );
 };
