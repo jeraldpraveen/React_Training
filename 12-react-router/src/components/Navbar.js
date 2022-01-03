91a0cacc548a5174e8e5f24ca624db2df0d694c2
jeraldpraveen <jerald.vit@gmail.com>
2022-01-03 16:32:00 +0530

    130.Link from React-Router

--- 12-react-router/src/components/Navbar.js
+++ 12-react-router/src/components/Navbar.js
@@ -1,13 +1,20 @@
 import React from "react";
 import "./Navbar.css";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div>
       <ul>
-        <li>Home</li>
-        <li>Contact Us</li>
-        <li>Services</li>
+        <Link to="/home">
+          <li>Home</li>
+        </Link>
+        <Link to="/contactus">
+          <li>Contact Us</li>
+        </Link>
+        <Link to="/services">
+          <li>Services</li>
+        </Link>
       </ul>
     </div>
   );
