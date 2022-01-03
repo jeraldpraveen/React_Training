0aa531e3bca09ea43154981466661faa12610b3e
jeraldpraveen <jerald.vit@gmail.com>
2022-01-03 16:50:50 +0530

    133.Loading Data using axios

--- 12-react-router/src/components/Services.js
+++ 12-react-router/src/components/Services.js
@@ -1,23 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 const Services = () => {
+  const [posts, setPosts] = useState([]);
+
   const getDataUsingAxios = () => {
     axios
       .get("https://jsonplaceholder.typicode.com/posts")
       .then((response) => {
-        console.log(response);
+        console.log(response.data);
+        setPosts(response.data);
       })
       .catch((error) => {
         console.log(error);
       });
   };
 
+  const titles = posts.map((post) => {
+    return (
+      <h1>
+        {post.id}:{post.title}
+      </h1>
+    );
+  });
+
   return (
     <div>
       <h1>Services</h1>
       <h1>HTTP METHODS</h1>
       <button onClick={getDataUsingAxios}>Get Data Using Axios</button>
+      {titles}
     </div>
   );
 };
