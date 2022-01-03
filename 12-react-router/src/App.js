7c2c1b77f5f210e89ca07b6fe535ad2d3bfee111
jeraldpraveen <jerald.vit@gmail.com>
2022-01-03 16:21:02 +0530

    128.BrowserRouter, Routes, & Route

--- 12-react-router/src/App.js
+++ 12-react-router/src/App.js
@@ -3,15 +3,20 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import ContactUs from "./components/ContactUs";
 import Services from "./components/Services";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <h1>REACT ROUTER</h1>
-      <Navbar />
-      <Home />
-      <ContactUs />
-      <Services />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/home" component={Home} exact />
+          <Route path="/contactus" component={ContactUs} exact />
+          <Route path="/services" component={Services} exact />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
