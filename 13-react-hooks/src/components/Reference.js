db2296cd6610db1c946250e1b2d0e21f79e1a547
jeraldpraveen <jerald.vit@gmail.com>
2022-03-11 03:49:25 +0530

    135.Modern Reference

--- 13-react-hooks/src/components/Reference.js
+++ 13-react-hooks/src/components/Reference.js
@@ -1,16 +1,27 @@
 import React, { Component } from "react";
 
 export class Reference extends Component {
-  onFocus = () => {
-    this.refs.text1.focus();
+  input = React.createRef();
+
+  onFocus = (value) => {
+    if (value === 1) {
+      this.refs.text1.focus();
+    } else {
+      this.input.current.focus();
+    }
   };
 
   render() {
     return (
       <div>
-        <input ref="text"></input>
-        <button onClick={this.onFocus}>Focus</button>
         <input ref="text1"></input>
+        <br />
+        <input ref={this.input}></input>
+        <br />
+        <button onClick={() => this.onFocus(1)}>Focus1</button>
+        <br></br>
+        <button onClick={() => this.onFocus(2)}>Focus2</button>
+        <br></br>
       </div>
     );
   }
